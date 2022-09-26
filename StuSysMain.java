c55d5b374b33febd9431f05cfe9540225081d0ff
Teck Yong <lty2004january@gmail.com>
2022-09-27 00:25:38 +0800

    Initial Framework

--- StuSysMain.java
+++ StuSysMain.java
@@ -1,9 +1,64 @@
+import java.util.Scanner;
+
 public class StuSysMain {
    public static void main(String[] args) {
-      StuSys stuSys = new StuSys();
-      stuSys.ImportAcct( "AcctInfo.txt" );
+      // StuSys stuSys = new StuSys();
+      // stuSys.ImportAcct( "AcctInfo.txt" );
+
+      boolean exit = false; // Sentinel value to control loop
+      Scanner input = new Scanner(System.in);
+      int option = -1;
+
+      // * MAIN SCREEN
+      // TODO: Print and prompt (either new, returning or exit)
+      // HINT: if .. else
+
+
+
+
+
 
       
+      if (option == 1){
+         // * Option1: New Student Screen
+         // TODO:  Print create account form and prompt user line by line
+
+
+
+      } else if (option == 2) {
+         // * Option 2: Returning Screen
+         // TODO: Print Login Option and promt user line by line. 
+
+      
+
+
+      } else if (option == 3) { 
+         // * Option 3: Exit
+         // TODO: Change exit to true
+
+      } else{  // if enter wrong number
+
+      }
+
+
+    
+
+
+
+      while (exit == false) {
+
+
+
+      }
+
+
+
+
+
+
+
+      
+
       
    }
 }
